26faf0ad7ccd33be7b635cef8b7f6da916df40e6
itsaraphap <itsaraphap.t@gmail.com>
2020-08-25 15:52:49 +0700

    ssss

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 $info = array(
-    'host' => '203.150.202.108',
+    'host' => '203.154.66.172',
     'user' => 'root',
     'password' => 'Icon@2020',
-    'dbname' => 'cm_prd'
+    'dbname' => 'cm_test'
 );
 $conn = mysqli_connect($info['host'], $info['user'], $info['password'], $info['dbname']) or die('Error connection database!');
 mysqli_set_charset($conn, 'utf8');
