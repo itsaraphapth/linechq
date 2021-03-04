ac7540efe8c72bb5337596986b8adf7e9cb35d61
itsaraphap <itsaraphap.t@gmail.com>
2021-03-04 14:21:01 +0700

    sss

--- line_uselib.php
+++ line_uselib.php
@@ -29,7 +29,7 @@ error_reporting(E_ALL);
  
 /// ส่วนการกำหนดค่านี้สามารถทำเป็นไฟล์ include แทนได้
 define('LINE_LOGIN_CHANNEL_ID','1655722109');
-define('LINE_LOGIN_CHANNEL_SECRET','89f1db727fa13f5566ff430c1901271b');
+define('LINE_LOGIN_CHANNEL_SECRET','aedd7e73357a47733335566dbaabe390');
 define('LINE_LOGIN_CALLBACK_URL','https://mk-line-api.herokuapp.com/login_uselib_callback.php');
  
 $LineLogin = new LineLoginLib(
