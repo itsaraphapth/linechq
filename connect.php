d179c85cc03361ecb0f0f6dd6c5bcab2953bcb6d
itsaraphap <itsaraphap.t@gmail.com>
2021-04-01 15:45:23 +0700

    update connection mk

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 $info = array(
-    'host' => '203.154.66.189',
+    'host' => '203.154.66.194',
     'user' => 'root',
-    'password' => 'wv8voc,o,kc]h;',
-    'dbname' => 'mk_uat'
+    'password' => 'Icon@2021',
+    'dbname' => 'mk_prd'
 );
 $conn = mysqli_connect($info['host'], $info['user'], $info['password'], $info['dbname']) or die('Error connection database!');
 mysqli_set_charset($conn, 'utf8');
